6c680158b701eae946ae23c2b3bef1b0cf3744b5
david <davidrios5424@gmail.com>
2024-03-23 03:42:00 -0700

    Initial commit

---
+++ src/main/java/staff/staffmode/StaffMode.java
@@ -0,0 +1,17 @@
+package staff.staffmode;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class StaffMode extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
